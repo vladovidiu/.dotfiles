beeedca15004c7c0b32f7281766b3aa97be55361
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2021-07-12 14:12:07 +0100

    add keybinds for gopls

--- neovim/.config/nvim/lua/lsp/nvim_go.lua
+++ neovim/.config/nvim/lua/lsp/nvim_go.lua
@@ -2,4 +2,17 @@ local go = require('go')
 local lspconfig = require('lspconfig')
 
 go.setup {}
-lspconfig.gopls.setup {}
+lspconfig.gopls.setup {
+  on_attach = function(_, bufnr)
+    local function buf_set_keymap(...)
+      vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+
+    local opts = {silent = true}
+
+    buf_set_keymap("n", "ct", ":GoTest<CR>", opts)
+    buf_set_keymap("n", "cgt", ":GoToTest<CR>", opts)
+    buf_set_keymap("n", "cf", ":Gofmt<CR>", opts)
+    buf_set_keymap("n", "cl", ":Golint<CR>", opts)
+  end
+}
