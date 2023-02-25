d1f9028e4fe7d780d9b5ae33fae7c343f55a77b8
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2023-02-25 17:49:36 +0000

    more fish goodies

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -1,3 +1,20 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    /usr/bin/rbenv init - fish | source
 end
+
+set -gx fish_greeting # disable fish greeting
+
+set -U EDITOR nvim # 'neovim/neovim' text editor 
+
+set -U FZF_CTRL_R_OPTS "--border-label=' History ' --prompt=' '"
+set -U FZF_DEFAULT_COMMAND "fd -H -E '.git'"
+set -U FZF_DEFAULT_OPTS "--reverse --no-info --prompt=' ' --pointer='' --marker=' ' --ansi --color gutter:-1,bg+:-1,header:4,separator:0,info:0,label:4,border:4,prompt:7,pointer:5,query:7,prompt:7"
+set -U FZF_TMUX_OPTS "-p --no-info --ansi --color gutter:-1,bg+:-1,header:4,separator:0,info:0,label:4,border:4,prompt:7,pointer:5,query:7,prompt:7"
+
+set -U GOPATH (go env GOPATH) # https://golang.google.cn/
+
+fish_add_path $GOPATH/bin
+
+starship init fish | source
+zoxide init fish | source
