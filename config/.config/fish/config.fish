bbfaebb476ab87c6f236134fb54324f80393c872
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2023-04-13 22:40:36 +0100

    fish + git config

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -7,6 +7,16 @@ set -gx fish_greeting # disable fish greeting
 
 set -U EDITOR nvim # 'neovim/neovim' text editor 
 
+# Set fish to use vi key bindings
+function fish_hybrid_key_bindings --description \
+"Vi-style bindings that inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+set -g fish_key_bindings fish_hybrid_key_bindings
+
 set -Ux MAKEFLAGS "-J"(nproc)
 
 set -U FZF_CTRL_R_OPTS "--border-label=' History ' --prompt=' '"
